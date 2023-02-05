cff38678f2eca980f6cb58dd8bbbbae667ef57bd
Daria <kotikgriga0027@gmail.com>
2023-02-05 13:25:39 +0300

    Solving problem in file ex_2

---
+++ ex_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Цикл for
+
+string Method4(int count, string text) {
+    string result = String.Empty; // пустая строка
+    for(int i = 0; i < count; i++) {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "pdf");
+Console.WriteLine(res);
