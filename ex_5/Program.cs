c881f373d810bff4d425ceafe1c39a1d32c89793
Daria <kotikgriga0027@gmail.com>
2023-02-10 13:14:27 +0300

    Solving problem in file ex_5

---
+++ ex_5/Program.cs
@@ -0,0 +1,31 @@
+﻿// sort
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array) {
+    int count = array.Length;
+    for (int i = 0; i < count; i++) {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array) {
+    for(int i = 0; i < array.Length - 1; i++) {
+        int minPos = i;  // позиция элемента ->
+        // Рассмотрим отсортированный массив
+        for(int j = i + 1; j < array.Length; j++) {
+            if(array[j] < array[minPos]) {
+                minPos = j;
+            }
+        }
+
+        int temp = array[i];
+        array[i] = array[minPos];
+        array[minPos] = temp;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
